4b047b1af646e72f0db09112c0a19c005ef7d9ac
Bruno Orosco <brunoorosco@gmail.com>
2026-04-10 10:23:43 -0300

    ajuste build

--- backend/tsup.config.ts
+++ backend/tsup.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/server.ts'],
   outDir: 'dist',
+  format: ['cjs'],
   target: 'es2022',
   clean: true,
   bundle: true,
   skipNodeModulesBundle: true,
-  external: ['@prisma/client', 'prisma', 'pino-pretty', 'pino-http'],
+  sourcemap: false,
+  external: ['@prisma/client', 'prisma'],
   esbuildOptions(options) {
     options.alias = {
       '@domain': './src/domain',
